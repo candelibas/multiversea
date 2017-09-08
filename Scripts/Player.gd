97d51def383c83a6f13c56a413e35756b7db7bdb
Can Delibas <candelibas96@gmail.com>
2017-09-09 00:31:50 +0300

    not fixed :(

---
+++ Scripts/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+onready var weapon = get_node("sprite/weapon_sprite") 
+var mouse_position
+
+func _ready():
+	set_fixed_process(true)
+	
+	
+func _fixed_process(delta):
+	# mouse_position = get_viewport().get_mouse_pos()
+	mouse_position = get_global_mouse_pos()
+	#weapon.look_at(Vector2(mouse_position.x * 5, mouse_position.y * 5 ))
+	weapon.set_rotd(-mouse_position.y / 5)
+	
+	# When character dies
+	#die()
+	
+func die():
+	weapon.translate(Vector2(1,1))
