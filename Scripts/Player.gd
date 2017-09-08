4e95a8ca92a2c48b20f505337680e293f220c567
Can Delibas <candelibas96@gmail.com>
2017-09-09 02:07:59 +0300

    add npc & move weapon according to mouse

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,10 +8,8 @@ func _ready():
 	
 	
 func _fixed_process(delta):
-	# mouse_position = get_viewport().get_mouse_pos()
 	mouse_position = get_global_mouse_pos()
-	#weapon.look_at(Vector2(mouse_position.x * 5, mouse_position.y * 5 ))
-	weapon.set_rotd(-mouse_position.y / 5)
+	weapon.look_at(mouse_position)
 	
 	# When character dies
 	#die()
