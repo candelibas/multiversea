28ed49f06331a6ddb2eee5f7b936eb3ce3ba1ce0
Can Delibas <candelibas96@gmail.com>
2017-09-09 05:05:09 +0300

    add mexico wall nd npcs to scene

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,16 +1,16 @@
 extends Node
 
-onready var npc_load = preload("res://Scenes/NPC.tscn")
-var new_npc
+var npc_load = preload("res://Scenes/NPC.tscn")
 
 func _ready():
 	set_fixed_process(true)
 	
-func _fixed_process(delta):
-	
-	# Spawn npcs
 	# Create starting npcs
-	for i in range(3):
-		new_npc = npc_load.instance()
+	for i in range(5):
+		var new_npc = npc_load.instance()
 		new_npc.set_pos(Vector2(1200, rand_range(30, 500)))
 		add_child(new_npc)
+	
+	
+func _fixed_process(delta):
+	
---
+++ Scripts/NPC.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+var vel = Vector2()
+var rot_speed
+
+func _ready():
+	randomize()
+	set_fixed_process(true)
+	
+	vel = Vector2(rand_range(-30, -50), 0)
+	rot_speed = rand_range(-0.2, 0.2)
+	
+func _fixed_process(delta):
+	set_rot(get_rot() + rot_speed)
+	move(vel * delta)
+	
+	if is_colliding():
+		self.queue_free()
