7925421954b154ce98ef79ca16c6fce3caf06c08
Can Delibas <candelibas96@gmail.com>
2017-09-10 02:00:46 +0300

    game fix

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,14 +3,13 @@ extends Node
 var npc_load = preload("res://Scenes/NPC.tscn")
 
 func _ready():
-	set_fixed_process(true)
 	
 	# Create starting npcs
 	for i in range(5):
 		var new_npc = npc_load.instance()
 		new_npc.set_pos(Vector2(1200, rand_range(10, 500)))
 		add_child(new_npc)
-	
-	
-func _fixed_process(delta):
-	
+
+
+func _on_Button_pressed():
+	get_tree().set_pause(true)
