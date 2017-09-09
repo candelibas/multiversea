d38977cd679763569ebec25a90d4a63986f9414c
Alseias <k_kurtulus@hotmail.com>
2017-09-09 04:40:41 +0300

    added bullets and fire

--- Scripts/player_bullet.gd
+++ Scripts/player_bullet.gd
@@ -12,6 +12,10 @@ func _ready():
 func start_at(dir, pos):
 	set_rot(dir)
 	set_pos(pos)
-	vel = Vector2(speed,0).rotated(dir)
+	vel = Vector2(speed,0).rotated(dir - PI/2)
 func _fixed_process(delta):
 	set_pos(get_pos() + vel * delta)
+
+
+func _on_lifetime_timeout():
+	queue_free()
