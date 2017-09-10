8cc436a8e03b15ee09d1bd8a617388799c5a49f3
Alseias <k_kurtulus@hotmail.com>
2017-09-10 05:04:21 +0300

    added score and star anim

--- Scripts/MexicanWall.gd
+++ Scripts/MexicanWall.gd
@@ -1,10 +1,14 @@
-extends Area2D
+extends KinematicBody2D
 
 onready var health_bar = get_tree().get_root().get_node("world/healthbar")
 
 func _ready():
+	add_to_group("wall")
 	pass
 
-func _on_mexican_wall_body_enter( body ):
-	health_bar.set_value(health_bar.get_value() - 50)
-	
+
+
+func _on_wall_body_enter( body ):
+	if body.get_groups().has("npc"):
+		health_bar.set_value(health_bar.get_value() - 50)
+	pass # replace with function body
--- Scripts/player_bullet.gd
+++ Scripts/player_bullet.gd
@@ -4,8 +4,12 @@ var vel = Vector2()
 export var speed = 1000
 onready var lod = preload("res://Scenes/Game.tscn")
 var nbr
+var score
 func _ready():
+	
 	nbr = lod.instance()
+	score = get_tree().get_root().get_node("world/label_score")
+	
 	set_fixed_process(true)
 
 func start_at(dir, pos):
@@ -20,7 +24,14 @@ func _on_lifetime_timeout():
 	queue_free()
 
 func _on_player_bullet_body_enter( body ):
+	#print(body.get_groups())
 	if body.get_groups().has("npc"):
 		body.destroy()
 		Global.explosion(get_pos())
+		var tempScore = str(int(score.get_text())+1)
+		#tempScore+1
+		
+		score.set_text(tempScore)
 		queue_free()
+	if body.get_name()=="mexican_wall":
+		vel=vel/2
